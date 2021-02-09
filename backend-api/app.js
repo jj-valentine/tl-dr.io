af031b38e29d2ef93803d4eea517b62359fd0fdd
JD <j@mesjohnson.com>
2021-02-08 23:01:40 -0800

    Prettify Files, Update NPM Packages, and Solve Post Model Search Issue + Add More Query Functionality

--- backend-api/app.js
+++ backend-api/app.js
@@ -1,27 +1,27 @@
-const express = require("express")
-const app = express()
-const sanitizeHTML = require("sanitize-html")
-const jwt = require("jsonwebtoken")
+const express = require("express");
+const app = express();
+const sanitizeHTML = require("sanitize-html");
+const jwt = require("jsonwebtoken");
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use("/", require("./router"))
+app.use("/", require("./router"));
 
-const server = require("http").createServer(app)
+const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   pingTimeout: 30000
-})
+});
 
-io.on("connection", function(socket) {
-  socket.on("chatFromClient", function(data) {
+io.on("connection", function (socket) {
+  socket.on("chatFromClient", function (data) {
     try {
-      let user = jwt.verify(data.token, process.env.JWTSECRET)
-      socket.broadcast.emit("chatFromServer", { text: sanitizeHTML(data.text, { allowedTags: [], allowedAttributes: {} }), username: user.username, avatar: user.avatar })
+      let user = jwt.verify(data.token, process.env.JWTSECRET);
+      socket.broadcast.emit("chatFromServer", { text: sanitizeHTML(data.text, { allowedTags: [], allowedAttributes: {} }), username: user.username, avatar: user.avatar });
     } catch (e) {
-      console.log("Not a valid token for chat.")
+      console.log("Not a valid token for chat.");
     }
-  })
-})
+  });
+});
 
-module.exports = server
+module.exports = server;
--- backend-api/controllers/followController.js
+++ backend-api/controllers/followController.js
@@ -1,25 +1,25 @@
-const Follow = require("../models/Follow")
+const Follow = require("../models/Follow");
 
-exports.apiAddFollow = function(req, res) {
-  let follow = new Follow(req.params.username, req.apiUser._id)
+exports.apiAddFollow = function (req, res) {
+  let follow = new Follow(req.params.username, req.apiUser._id);
   follow
     .create()
     .then(data => {
-      res.json(data)
+      res.json(data);
     })
     .catch(errors => {
-      res.json(false)
-    })
-}
+      res.json(false);
+    });
+};
 
-exports.apiRemoveFollow = function(req, res) {
-  let follow = new Follow(req.params.username, req.apiUser._id)
+exports.apiRemoveFollow = function (req, res) {
+  let follow = new Follow(req.params.username, req.apiUser._id);
   follow
     .delete()
     .then(() => {
-      res.json(true)
+      res.json(true);
     })
     .catch(errors => {
-      res.json(false)
-    })
-}
+      res.json(false);
+    });
+};
